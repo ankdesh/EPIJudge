0e5f3a4c9c984b36b84a17aab0bb3a8da1203be1
ankdesh <a.s.deshwal@gmail.com>
2018-11-01 11:43:32 +0530

    Solve search_first_key from searching chapter

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -4,7 +4,34 @@ using std::vector;
 
 int SearchFirstOfK(const vector<int>& A, int k) {
   // TODO - you fill in here.
-  return 0;
+
+  int l = 0;
+  int r = A.size() - 1;
+  int found = -1;
+
+  while (l <= r){
+    int m = l + (r-l)/2;
+    if (k < A[m]){
+      r = m - 1;
+    }
+    else if (k > A[m]){
+      l = m + 1;
+    }
+    else {
+      found = m;
+      break;
+    }
+  }
+
+  if (found != -1){
+    while (found > 0 && A[found] == A[found-1]){
+      found--;
+    }
+    return found;
+  }
+  else {
+    return -1;
+  }
 }
 
 int main(int argc, char* argv[]) {
